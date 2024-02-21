cc877fc7fcde0ddff31e4a8ebd47f75cc9899371
EDOHWARES <116767410+EDOHWARES@users.noreply.github.com>
2024-02-22 00:06:48 +0100

    test file rendered inactive

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
-def prime_factors(factors):
-    prime = []
-    for el in factors:
-        for i in range(2, el+1):
-            k = []
-            if el % i == 0:
-                k.append(i)
-    prime.append(k)
+# def prime_factors(factors):
+#     prime = []
+#     for el in factors:
+#         for i in range(2, el+1):
+#             k = []
+#             if el % i == 0:
+#                 k.append(i)
+#     prime.append(k)
 
-    return prime
+#     return prime
 
 
-k = [3, 4, 5, 6, 7, 8, 9, 10]
+# k = [3, 4, 5, 6, 7, 8, 9, 10]
 
-print(prime_factors(k))
+# print(prime_factors(k))
 
 
 
